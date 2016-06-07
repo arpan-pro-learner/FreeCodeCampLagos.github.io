957dcb3039802ea3b2f729b39fa01d2e8cb93b94
Hector101 <hector1014really32@gmail.com>
2016-06-07 15:07:38 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,19 +18,18 @@ $(document).ready(function(){
 
     // Add smooth scrolling to all links in navbar + footer link
     $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-
+        
         // Prevent default anchor click behavior
         event.preventDefault();
-
+        
         // Store hash
         var hash = this.hash;
-
+        
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
             scrollTop: $(hash).offset().top
         }, 900, function(){
-
             // Add hash (#) to URL when done scrolling (default click behavior)
             window.location.hash = hash;
         });
@@ -41,9 +40,13 @@ $(document).ready(function(){
         // check if user scrolled more than 550 from top of the browser window
         if ($(document).scrollTop() > 550) {
             // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-            $(".navbar-fixed-top").css("background-color", "black");
-        } else {
+            $(".navbar-fixed-top").css("background-color", "#0e820e");
+        } 
+        else {
             $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+            
         }
     });
+    
+   
 });
